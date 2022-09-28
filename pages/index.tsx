e9e557708d7e69d54b345a20c8357c7ba823f611
Patrick Coutinho <patrick@istyle.com.br>
2022-09-28 12:00:50 -0300

    feat: created supabase service

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from "next";
+import { supabase } from "./services/supabase";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ lessons }: any) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      working
+      {lessons.map((lesson: any) => (
+        <p key={lesson.id}>{lesson.title}</p>
+      ))}
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const { data: lessons } = await supabase.from("lesson").select("*");
+
+  return { props: { lessons } };
 };
 
 export default Home;
